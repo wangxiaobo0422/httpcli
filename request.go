1d9f07649a20de1962f38d3603a856f33d5b151f
xiaobo.wang <wangxiaobo@qq.com>
2025-06-04 00:02:35 +0800

    添加request文件

---
+++ request.go
@@ -0,0 +1,29 @@
+package httpcli
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"sync"
+)
+
+type request struct {
+	*http.Request
+	reader       io.Reader
+	rawBody      []byte
+	readBodyErr  error
+	readBodyOnce sync.Once
+}
+
+type RequestOption func(req *request)
+
+func WrapRequest(req *http.Request) *request {
+	return &request{
+		Request: req,
+		reader:  req.Body,
+	}
+}
+
+func NewRequest(ctx context.Context, method, url string, body io.Reader, opts ...RequestOption) (*request, error) {
+
+}
